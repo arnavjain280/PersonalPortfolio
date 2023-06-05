ba0786be7043cb2eb10f011ddef108fe317eab75
Arnav Jain <arnav.jain823@gmail.com>
2023-06-05 12:09:14 -0600

    initail body

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import {motion} from 'framer-motion';
+import {styles} from '../styles';
+import { ComputersCanvas } from './canvas';
+
+
+// const Hero = () => {
+//   return (
+//     <section className=" w-full h-screen
+//      mx-auto">
+//       <div className={`${styles.paddingX} absolute inset-0 
+//       top-[120px] max-w-7xl mx-auto flex flex-row justify-between items-start gap-5`}>
+//           <div className='flex flex-col '>
+
+//           </div>
+//       </div>
+
+//      </section>
+//   )
+// }
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className={`relative w-full h-screen mx-auto`}>
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+          <div className='w-1 sm:h-80 h-40 violet-gradient' />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className='text-[#915EFF]'>Arnav</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            CS @ CU Boulder <br className='sm:block hidden' />
+            Interested in AI, ML, and Cybersecurity
+          </p>
+        </div>
+      </div>
+
+      <ComputersCanvas />
+
+      
+    </section>
+  );
+};
+
 
 export default Hero
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 import { styles } from "../styles";
 import { navLinks } from "../constants";
-import {logo, menu, close } from "../assets";
-// import logo from "../assets/a-logo.png";
+import {  menu, close } from "../assets";
+import logo from "../assets/a-logo.png";
 
 
 
@@ -13,22 +13,22 @@ const Navbar = () => {
   const [toggle, setToggle] = useState(false);
   return (
     <nav
-    // take full width, centered, stay on the top and appear above other elements
-    className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      // take full width, centered, stay on the top and appear above other elements
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
           to="/" //changed from to="/'"
           className="flex items-center gap-2"
-          onClick={()=>{
+          onClick={() => {
             setActive(""); //where we are in the page
-            window.scrollTo(0,0); //scroll to top
+            window.scrollTo(0, 0); //scroll to top
           }}
-          >
+        >
           <img src={logo} alt="a-logo" className="w-9 h-9 object contain" />
           <p className="text-white text-[18px] 
           font-bold cursor-pointer ">Arnav Jain
-          {/* <span 
+            {/* <span 
           className="sm:block hidden"> |
             Javascript Mastery
           </span> */}
@@ -39,34 +39,50 @@ const Navbar = () => {
           {navLinks.map((link) => (
             <li
               key={link.id}
-              className={`${
-                active === link.title
-                ? "text-white"
-                : "text-secondary"
-              } hover:text-white text-[18px] font-bold cursor-pointer transition-all duration-300 font-medium  `}
+              className={`${active === link.title
+                  ? "text-white"
+                  : "text-secondary"
+                } hover:text-white text-[18px] font-bold cursor-pointer transition-all duration-300 font-medium  `}
               onClick={() => setActive(link.title)}
             >
               <a href={`#${link.id}`}>
-              {link.title}
+                {link.title}
               </a>
             </li>
           ))}
         </ul>
 
         <div className='sm:hidden flex flex-1 justify-end items-center'>
-            <img
-              src={toggle ? close : menu}
-              alt='menu'
-              className='w-[28px] h-[28px] object-contain'
-              onClick={() => setToggle(!toggle)}
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
           />
-          <div className={`${
-              !toggle ? "hidden" : "flex"} 
+          <div className={`${!toggle ? "hidden" : "flex"} 
               p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
-            
-          </div>
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                    } hover:text-white text-[18px] font-poppins font-bold cursor-pointer transition-all duration-300 font-medium  `}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title)}
+                  }
+                >
+                  <a href={`#${link.id}`}>
+                    {link.title}
+                  </a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
+      </div>
 
     </nav>
   )
