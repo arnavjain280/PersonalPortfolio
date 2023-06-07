412df3a52711463433bc8a1131247ddcd0488d8d
Arnav Jain <arnav.jain823@gmail.com>
2023-06-06 18:37:08 -0600

    model load percentage

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,9 +2,19 @@ import { Html, useProgress} from "@react-three/drei"
 const Loader = () => {
   const {progress} = useProgress();
   return (
+    // show progress of the load */}
     <Html>
       <span className="canvas-load"> </span>
-      <p>{progress.toFixed(2)}%</p>
+      <p
+        className="canvas-load"
+        style={{
+          fontSize: 30,
+          color: "#915EFF",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%</p> 
     </Html>
   )
 }
