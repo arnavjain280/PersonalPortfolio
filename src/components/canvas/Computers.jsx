164f67672c55ef28968877c0026ea47821335759
Arnav Jain <arnav.jain823@gmail.com>
2023-06-07 13:34:12 -0600

    fixed check for movile devices

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
   return (
     <mesh>
@@ -18,9 +18,11 @@ const Computers = () => {
          castShadow
          shadow-mapSize={2048}
          />
-      <primitive object={computer.scene} scale={0.75}
-      position={[0,-3.25,-1.5]} 
-      rotation = {[-0.01,-0.2,-0.1]}
+      <primitive 
+        object={computer.scene} 
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        rotation = {[-0.01,-0.2,-0.1]}
       />
     </mesh>
   )
@@ -63,7 +65,7 @@ const ComputersCanvas = () => {
            maxPolarAngle={Math.PI/2}
            minPolarAngle={Math.PI/2}
           />
-          <Computers />
+          <Computers isMobile={isMobile} />
          </Suspense>
          <Preload all />
       
