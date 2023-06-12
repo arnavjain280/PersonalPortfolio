f751c9dbe192c4fc41da272339e46e9222301d6a
Arnav Jain <arnav.jain823@gmail.com>
2023-06-11 18:01:38 -0600

    added project images revised contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -22,8 +22,21 @@ const Contact = () => {
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div 
-      // variants={}
+      variants={slideIn('left', "tween",0.2,1)}
+      className="flex-[0.75] bg-black-100 p-8 rounded-2xl "
       >
+        <p className={styles.sectionSubText}>
+          Get in Touch</p>
+        <h3 className={styles.sectionHeadText}>
+          Contact
+        </h3>
+        <form
+        ref={formRef}
+        onSubmit={handleSubmit}
+        classname= "mt-12 flex flex-col gap-8"     >
+
+        </form>
+
 
       </motion.div>
     </div>
